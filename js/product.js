72bdfcde7e0f29d7df0655aa9ef21765d7454ac7
Emili <ejanchevis@gmail.com>
2023-12-19 11:36:31 -1000

    size button toggle-able

--- js/product.js
+++ js/product.js
@@ -11,3 +11,16 @@ productImages.forEach((item, i) => { // loopinh through each image thumb
         activeImageSlide = i; // updating the image slider variable to track current thumb
     })
 })
+
+// toggle size buttons
+
+const sizeBtns = document.querySelectorAll('.size-radio-btn'); // selecting size buttons
+let checkedBtn = 0; // current selected button
+
+sizeBtns.forEach((item, i) => { // looping through each button
+    item.addEventListener('click', () => { // adding click event to each 
+        sizeBtns[checkedBtn].classList.remove('check'); // removing check class from the current button
+        item.classList.add('check'); // adding check class to clicked button
+        checkedBtn = i; // upading the variable
+    })
+})
